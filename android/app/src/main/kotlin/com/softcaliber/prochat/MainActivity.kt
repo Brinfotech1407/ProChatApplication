35a122511d84e974c3c1afab0cc8af47d80cc638
bipin <harsh.agrawal@twinsway.com>
2022-11-02 10:55:05 +0530

    #Initial Commit.

---
+++ android/app/src/main/kotlin/com/softcaliber/prochat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.softcaliber.prochat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Utils/setStatusBarColor.dart
@@ -0,0 +1,9 @@
+import 'package:prochat/Configs/app_constants.dart';
+import 'package:flutter/services.dart';
+
+setStatusBarColor() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: statusBarColor,
+      statusBarIconBrightness:
+          isDarkIconsinStatusBar ? Brightness.dark : Brightness.light));
+}
